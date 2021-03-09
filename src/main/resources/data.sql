b47d884ee3f21764d82bed2e99ef6dce65558102
Gabriel Brandão <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-09 19:37:48 -0300

    chore: add sql seed to students table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_student (enrollment, name, last_name) VALUES ('1234', 'Gabriel', 'Brandao');
+INSERT INTO tb_student (enrollment, name, last_name) VALUES ('4321', 'Carol', 'Martins');
+INSERT INTO tb_student (enrollment, name, last_name) VALUES ('5678', 'Pedro', 'Benicio');
+INSERT INTO tb_student (enrollment, name, last_name) VALUES ('7658', 'Alexandre', 'Silva');
+INSERT INTO tb_student (enrollment, name, last_name) VALUES ('9090', 'Kelly', 'Costa');
