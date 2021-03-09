fc4a4580eca22c750410bc1c05b6230afeafeb7f
Gabriel Brandão <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-09 19:06:30 -0300

    chore: Project created with basic dependencies

---
+++ src/main/java/com/devgabriel/challengeforleven/ChallengeForlevenApplication.java
@@ -0,0 +1,13 @@
+package com.devgabriel.challengeforleven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForlevenApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChallengeForlevenApplication.class, args);
+  }
+
+}
