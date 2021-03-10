87b998867ce0f27fb9b3c8d7411a74a45c449021
Gabriel Brandão <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-10 09:49:15 -0300

    chore: add sql seed for phone table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,8 @@ INSERT INTO tb_student (enrollment, name, last_name) VALUES ('4321', 'Carol', 'M
 INSERT INTO tb_student (enrollment, name, last_name) VALUES ('5678', 'Pedro', 'Benicio');
 INSERT INTO tb_student (enrollment, name, last_name) VALUES ('7658', 'Alexandre', 'Silva');
 INSERT INTO tb_student (enrollment, name, last_name) VALUES ('9090', 'Kelly', 'Costa');
+
+INSERT INTO tb_phone (phone_number, student_enrollment) VALUES ('97765-0983', '4321');
+INSERT INTO tb_phone (phone_number, student_enrollment) VALUES ('95564-1231', '4321');
+INSERT INTO tb_phone (phone_number, student_enrollment) VALUES ('95432-7103', '7658');
+INSERT INTO tb_phone (phone_number, student_enrollment) VALUES ('96634-5041', '7658');
