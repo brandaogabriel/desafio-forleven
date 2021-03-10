1a706cfb20da8e1dfb220db3377cbad73c99297c
Gabriel Brandão <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-10 20:45:50 -0300

    test: add integration test StudentServiceIT

--- src/test/java/com/devgabriel/challengeforleven/tests/factory/StudentFactory.java
+++ src/test/java/com/devgabriel/challengeforleven/tests/factory/StudentFactory.java
@@ -1,20 +1,24 @@
 package com.devgabriel.challengeforleven.tests.factory;
 
-import com.devgabriel.challengeforleven.dtos.StudentDTO;
 import com.devgabriel.challengeforleven.dtos.StudentInsertDTO;
+import com.devgabriel.challengeforleven.dtos.StudentUpdateDTO;
 import com.devgabriel.challengeforleven.entities.Phone;
 import com.devgabriel.challengeforleven.entities.Student;
 
 public class StudentFactory {
 
   public static Student createStudent() {
-    Student student = new Student("1234", "Robert", "Martin");
+    Student student = new Student("5544", "Jacob", "Woods");
     student.getPhoneNumbers().add(new Phone(null, "99988-7744"));
     return student;
   }
 
-  public static StudentDTO createStudentDTO() {
-    return new StudentDTO(createStudent());
+  public static StudentUpdateDTO createStudentUpdateDTO() {
+    StudentUpdateDTO dto = new StudentUpdateDTO();
+    Student student = createStudent();
+    dto.setName(student.getName());
+    dto.setLastName(student.getLastName());
+    return dto;
   }
 
   public static StudentInsertDTO createStudentInsert() {
---
+++ src/test/java/com/devgabriel/challengeforleven/tests/integration/StudentServiceIT.java
@@ -0,0 +1,92 @@
+package com.devgabriel.challengeforleven.tests.integration;
+
+import com.devgabriel.challengeforleven.dtos.StudentDTO;
+import com.devgabriel.challengeforleven.dtos.StudentInsertDTO;
+import com.devgabriel.challengeforleven.dtos.StudentUpdateDTO;
+import com.devgabriel.challengeforleven.services.StudentService;
+import com.devgabriel.challengeforleven.services.exceptions.ResourceNotFoundException;
+import com.devgabriel.challengeforleven.tests.factory.StudentFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@SpringBootTest
+@Transactional
+class StudentServiceIT {
+
+  @Autowired
+  private StudentService service;
+
+  private String existingEnrollment;
+  private String nonExistingEnrollment;
+  private long countTotalStudents;
+  private StudentUpdateDTO existingStudent;
+  private StudentInsertDTO newStudentInsertDTO;
+  private String expectedStudentName;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    existingEnrollment = "1234";
+    nonExistingEnrollment = "6789";
+    countTotalStudents = 5L;
+    existingStudent = StudentFactory.createStudentUpdateDTO();
+    newStudentInsertDTO = StudentFactory.createStudentInsert();
+    expectedStudentName = "Jacob";
+  }
+
+  @Test
+  void findAllShouldReturnStudents() {
+    List<StudentInsertDTO> students = service.findAll();
+    Assertions.assertFalse(students.isEmpty());
+    Assertions.assertEquals(countTotalStudents, students.size());
+  }
+
+  @Test
+  void findByEnrollmentShouldReturnStudentDTOWhenEnrollmentExists() {
+    String expectedName = "Gabriel";
+    StudentDTO studentDTO = service.findByEnrollment(existingEnrollment);
+    Assertions.assertNotNull(studentDTO);
+    Assertions.assertEquals(expectedName, studentDTO.getName());
+  }
+
+  @Test
+  void findByEnrollmentShouldThrowsResourceNotFoundExceptionWhenEnrollmentDoesNotExists() {
+    Assertions.assertThrows(ResourceNotFoundException.class, () -> service.findByEnrollment(nonExistingEnrollment));
+  }
+
+  @Test
+  void insertShouldReturnStudentDTOWhenDataIsValid() {
+    StudentInsertDTO dto = newStudentInsertDTO;
+    StudentDTO studentDTO = service.insert(dto);
+    Assertions.assertNotNull(studentDTO);
+    Assertions.assertEquals(dto.getName(), studentDTO.getName());
+  }
+
+  @Test
+  void updateShouldReturnStudentDTOWhenEnrollmentExists() {
+    service.update(existingEnrollment, existingStudent);
+    Assertions.assertNotNull(existingStudent);
+    Assertions.assertEquals(expectedStudentName, existingStudent.getName());
+  }
+
+  @Test
+  void updateShouldThrowsResourceNotFoundExceptionWhenEnrollmentDoesNotExists() {
+    Assertions.assertThrows(ResourceNotFoundException.class, () -> service.update(nonExistingEnrollment, existingStudent));
+  }
+
+  @Test
+  void deleteShouldDoNothingWhenEnrollmentExists() {
+    Assertions.assertDoesNotThrow(() -> service.delete(existingEnrollment));
+  }
+
+  @Test
+  void deleteShouldThrowsResourceNotFoundExceptionWhenEnrollmentDoesNotExists() {
+    Assertions.assertThrows(ResourceNotFoundException.class, () -> service.delete(nonExistingEnrollment));
+  }
+}
